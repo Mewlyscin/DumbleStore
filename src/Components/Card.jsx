4a1400dea3b61b15237d9b46d7bb51d9d09cb005
Mewlyscin <Tear-Lyons7@live.fr>
2023-10-12 17:42:18 +0200

    modif css & rajout money & shop

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,16 +1,27 @@
-function Card({ image, title, description, key }) {
+function Card({ image, title, description, key, gallions, mornilles }) {
   return (
     <div className="card" id={key}>
-      <img className="card-img" src={image} alt="" />
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <button>
-        <img
-          src="https://img.icons8.com/ios-filled/50/shopping-basket.png"
-          alt="shopping-basket"
-          className="panier-icon"
-        />
-      </button>
+      <span>
+        <img className="card-img" src={image} alt="" />
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </span>
+      <span>
+        <p className="price">
+          <img className="money" src="../src/Img/gallion.webp" />
+          &nbsp;{gallions}
+          &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <img className="money" src="../src/Img/mornille.webp" />
+          &nbsp;{mornilles}
+        </p>
+        <button>
+          <img
+            src="https://img.icons8.com/ios-filled/50/shopping-basket.png"
+            alt="shopping-basket"
+            className="panier-icon"
+          />
+        </button>
+      </span>
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,21 @@
 function Navbar() {
   return (
     <navbar>
-      <img src="./src/img/logo.jpg" alt="logo" />
-      <h1>DumbleStore</h1>
+      <div className="logo">
+        <img src="./src/img/logo.jpg" alt="logo" />
+      </div>
+      <>
+        <h1>DumbleStore</h1>
+      </>
+
+      <div className="shop">
+        <span className="achat">
+          <img></img> <p>tutututut</p>
+        </span>
+        <span className="total">
+          <img></img> <p>tototototo</p>
+        </span>
+      </div>
     </navbar>
   );
 }
