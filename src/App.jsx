f99c4664ca853ac6f434df2479b5c8f287515d43
Mewlyscin <Tear-Lyons7@live.fr>
2023-10-12 10:05:24 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import CardList from "./Components/CardList";
+import "./App.css";
+
+function App() {
+  const [, set] = useState(0);
+
+  return <></>;
+}
+
+export default App;
---
+++ src/Components/CardList.jsx
@@ -0,0 +1,7 @@
+import data from "./data/data.json";
+import Card from "./Card";
+
+const CardList() {
+};
+
+export default CardList;
