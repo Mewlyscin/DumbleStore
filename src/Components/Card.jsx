ee5e0a5a1713e30aa3b44df5b03382e7565f089b
Pierre Arnaudet <pierre.arnaudet44@gmail.com>
2023-10-12 22:11:19 +0200

    fix conflicts

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,28 +1,33 @@
 function Card({ image, title, description, key, gallions, mornilles }) {
   return (
     <div className="card" id={key}>
-      <span>
-        <img className="card-img" src={image} alt="" />
-
+      <div>
+        <img
+          className="card-img"
+          src={image}
+          alt=""
+          width="320px"
+          height="300px"
+        />
         <h2>{title}</h2>
         <p>{description}</p>
-      </span>
-      <span>
-        <p className="price">
+      </div>
+      <div className="shop-card">
+        <span className="price">
           <img className="money" src="../src/Img/gallion.webp" />
           &nbsp;{gallions}
-          &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <img className="money" src="../src/Img/mornille.webp" />
           &nbsp;{mornilles}
-        </p>
-        <button>
+        </span>
+        <button className="basket-button">
           <img
             src="https://img.icons8.com/ios-filled/50/shopping-basket.png"
             alt="shopping-basket"
             className="panier-icon"
           />
         </button>
-      </span>
+      </div>
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,14 @@
 function Navbar() {
   return (
     <navbar>
-      <div className="logo">
-        <img src="./src/img/logo.jpg" alt="logo" />
-      </div>
-      <>
-        <h1>DumbleStore</h1>
-      </>
-
+      <img src="./src/img/logo.jpg" alt="logo" />
+      <h1>DumbleStore</h1>
       <div className="shop">
         <span className="achat">
-          <img className="caddy" src=""></img> <p>tutututut</p>
+          <p>🧺 tutututut</p>
         </span>
         <span className="total">
-          <img className="caddy" src=""></img> <p>tototototo</p>
+          <p>🧾 tototototo</p>
         </span>
       </div>
     </navbar>
