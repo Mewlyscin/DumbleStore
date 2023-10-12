9a3a20df34e623debce51a3617e821912e314824
Pierre Arnaudet <pierre.arnaudet44@gmail.com>
2023-10-12 14:15:16 +0200

    add img logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,7 @@
 function Navbar() {
   return (
     <navbar>
-      <img
-        src="https://cdn.discordapp.com/attachments/1149632412146483240/1161950807697149993/coleka-plastoy-figurine-dumblestore-et-fumsck_470x246.png?ex=653a2a04&is=6527b504&hm=1b4f76ffe20bfded67f3e72ac4149b59c6da38e57a82b68cf8fab929548ac07f&"
-        alt="logo"
-      />
+      <img src="./src/img/logo.jpg" alt="logo" />
       <h1>DumbleStore</h1>
     </navbar>
   );
